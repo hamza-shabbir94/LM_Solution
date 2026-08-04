41829272df37dc9a555808028ccfd3e4b6d8c2c8
hamza-shabbir94 <hamza.shabbir.94.4@gmail.com>
2026-08-05 01:32:06 +0200

    fix: upgrading hashicorp/aws version

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -2,11 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0"
+      version = "~> 6.0"
     }
   }
 }
