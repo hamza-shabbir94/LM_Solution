10013717c181de63535e8041e41ec7da786f0e48
hamza-shabbir94 <hamza.shabbir.94.4@gmail.com>
2026-08-05 16:00:52 +0200

    Chores: adding kubernetes manifest files

---
+++ command.sh
@@ -0,0 +1,3 @@
+docker login -u <username>
+docker tag api-service:latest hamza94/production-api-service:1.0.0
+docker push hamza94/production-api-service:1.0.0
--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -129,11 +129,4 @@ module "eks" {
 }
 
 
-resource "aws_ecr_repository" "api_service" {
-  name                 = "${var.environment}-api-service"
-  image_tag_mutability = "IMMUTABLE" 
 
-  image_scanning_configuration {
-    scan_on_push = true 
-  }
-}
