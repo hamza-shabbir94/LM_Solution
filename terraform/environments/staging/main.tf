10c675de009b8f4072c74bc257cdf163e7cf60f1
hamza-shabbir94 <hamza.shabbir.94.4@gmail.com>
2026-08-06 10:04:31 +0200

    fix: instance type in staging

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -19,7 +19,7 @@ module "platform" {
   aws_region  = "eu-central-1"
   vpc_cidr    = "10.1.0.0/16"
 
-  node_instance_types = ["t3.medium"]
+  node_instance_types = ["t3.small"]
   node_min_size       = 2
   node_max_size       = 5
   node_desired_size   = 2
